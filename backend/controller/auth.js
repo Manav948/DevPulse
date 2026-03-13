a57e7fd723543f7200e65f4c5f30468a83816588
Manav948 <valanimanav61@gmail.com>
2026-03-13 23:17:12 +0530

    update signup and signin route

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -5,39 +5,36 @@ import bcrypt from "bcryptjs"
 export const signUp = async (req, res) => {
 
     try {
-        const { username, email, password, profileImage, provider, providerId, isVerified, role, lastLogin } = req.body
+        const { username, email, password } = req.body
         const existingUser = await User.findOne({ email: email })
 
         if (existingUser) {
             return res.status(409).json({ message: 'User Already Exist' })
         }
-        if (password.length < 6) {
+        if (password.length < 6 || !password) {
             return res.status(400).json({ message: 'Password Must be 6 Digit' })
         }
 
         const hashedPassword = await bcrypt.hash(password, 12);
-        const user = new User({
+
+        const user = await User.create({
             username,
             email,
             password: hashedPassword,
-            profileImage,
-            provider,
-            providerId,
-            isVerified,
-            role,
-            lastLogin
+            provider: "local"
         })
-        await user.save();
+
         const token = generateToken(user);
         res.status(201).json({
-            message: "User registered successfully",
+            message : "User registered successfully",
             token,
-            user: {
+            user : {
                 id: user._id,
-                username: user.username,
-                email: user.email
+                username : user.username,
+                email : user.email
+
             }
-        })
+        })   
     } catch (error) {
         console.log("Error in SignUp Router")
         res.status(500).json({ message: "Server error", error: error.message });
@@ -47,10 +44,16 @@ export const signIn = async (req, res) => {
 
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email }).select("+password");
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
+        }
+
+        if(user.provider !== "local"){
+            return res.status(400).json({
+                message : "Please login using Google"
+            })
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
@@ -59,7 +62,11 @@ export const signIn = async (req, res) => {
             return res.status(401).json({ message: "Invalid credentials" });
         }
 
+        user.lastLogin  = new Date();
+        await user.save()
+        
         const token = generateToken(user)
+
         res.status(200).json({
             message: "Login successful",
             token,
