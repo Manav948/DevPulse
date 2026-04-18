f754b8efeae8a372f58cf548d99455de072c68b2
Manav948 <valanimanav61@gmail.com>
2026-04-18 23:56:31 +0530

    update middleware and implement reate limiting

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,20 +3,26 @@ import jwt from "jsonwebtoken"
 export const isAuth = (req, res, next) => {
     try {
         let token;
-
-        if (req.cookies?.token) {
-            token = req.cookies.token
-        }
-        if (!token && req.headers.authorization?.startsWith("Bearer")) {
+        if (req.headers.authorization?.startsWith("Bearer ")) {
             token = req.headers.authorization.split(" ")[1];
         }
 
+        if (!token && !req.cookies?.accessToken) {
+            token = req.cookies.accessToken;
+        }
         if (!token) {
             return res.status(401).json({ message: "Unauthorized access" });
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = decoded
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (decoded.type && decoded.type !== "access") {
+            return res.status(401).json({ message: "Invalid token type" });
+        }
+        req.user = {
+            id : decoded.id,
+            email : decoded.email,
+            role : decoded.role
+        }
         next()
     } catch (error) {
         console.log("Authentication error : ", error)
